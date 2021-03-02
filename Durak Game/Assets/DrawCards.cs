8cf5b71a2ad22bf5b24558dcda1db30cae6f5659
S.M.HASIBUR RAHMAN <30457341+Hasib001@users.noreply.github.com>
2021-03-02 13:02:24 -0500

    Script added for Draw card button

---
+++ Durak Game/Assets/DrawCards.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour {
+
+
+	public GameObject card1;
+	public GameObject card2;
+	public GameObject playerArea;
+	public GameObject aiArea;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void OnClick()
+    {
+		GameObject playerCard = Instantiate(card1, new Vector3(0, 0, 0), Quaternion.identity);
+		playerCard.transform.SetParent(playerArea.transform, false);
+	}
+}
